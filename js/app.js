21238e4b99b479f3ae154324b3c59f99b38d9aa2
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2017-12-22 08:25:16 +0700

    Insert data for Review

--- js/app.js
+++ js/app.js
@@ -17,38 +17,67 @@
 
    var gems = [
         {
-            name : 'Dodecahedron',
-            price : 2.95,
-            description : 'abc xzy 0',
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: 'abc xzy 0',
             images: [
                 {
-                    full : 'gem-01-full.jpg',
-                    thumb : 'gem-01-thumb.gif'
+                    full: 'gem-01-full.jpg',
+                    thumb: 'gem-01-thumb.gif'
                 },
                 {
-                    full : 'gem-02-full.jpg',
-                    thumb : 'gem-02-thumb.png'
+                    full: 'gem-02-full.jpg',
+                    thumb: 'gem-02-thumb.png'
                 }
             ],
-            canPurchase : true,
-            soldOut : false
+            reviews: [
+                {
+                    stars: 5, 
+                    body: "I love this product!",  
+                    author: "joe@thomas.com"
+                },
+                {
+                    stars: 1, 
+                    body: "This product sucks",
+                    author: "tim@hater.com" 
+                }
+            ],
+            canPurchase: true,
+            soldOut: false
         },
         {
-            name : 'Pentagonal Gem',
-            price : 5.95,
-            description : 'abc xzy 0',
+            name: 'Pentagonal Gem',
+            price: 5.95,
+            description: 'abc xzy 0',
             images: [
                 {
-                    full : 'gem-01-full.jpg',
-                    thumb : 'gem-01-thumb.gif'
+                    full: 'gem-01-full.jpg',
+                    thumb: 'gem-01-thumb.gif'
                 },
                 {
-                    full : 'gem-02-full.jpg',
-                    thumb : 'gem-02-thumb.png'
+                    full: 'gem-02-full.jpg',
+                    thumb: 'gem-02-thumb.png'
                 }
             ],
-            canPurchase : true,
-            soldOut : false
+            reviews: [
+                {
+                    stars: 5, 
+                    body: "I love this product!",  
+                    author: "joe@thomas.com" 
+                },
+                {
+                    stars: 1, 
+                    body: "This product sucks",
+                    author: "tim@hater.com" 
+                },
+                {
+                    stars: 3, 
+                    body: "This product OK",
+                    author: "admin@hater.com" 
+                }
+            ],
+            canPurchase: true,
+            soldOut: false
         }
     ];
 })();
